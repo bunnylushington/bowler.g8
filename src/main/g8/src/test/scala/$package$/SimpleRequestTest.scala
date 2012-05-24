6de560a163157f440ec37f56aff5e06ecb351b10
Kevin Montuori <kevin.montuori@pearson.com>
2012-05-24 10:45:14 -0400

    fixed directory name typo; tweaked a little indentation

--- src/main/g8/src/test/scala/$package$/SimpleRequestTest.scala
+++ src/main/g8/src/test/scala/$package$/SimpleRequestTest.scala
@@ -9,12 +9,12 @@ class SimpleRequestTest extends ScalatraFunSuite{
   val holder = this.addFilter(classOf[BowlerFilter], "/*")
 
   test("Simple Controller Route"){
-	// let's run the bootstrap of the app
+    // let's run the bootstrap of the app
     val bootstrap = new Bootstrap
     get("/"){
-	 println(body)
-	  // lets just check that the body of the response contains
-	  // the text in the index.mustache view file.
+      println(body)
+      // lets just check that the body of the response contains
+      // the text in the index.mustache view file.
       assert(body.contains("This is the index view!"))
     }
   }
